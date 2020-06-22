dc85881fbbe85d06d9811569cb66a74dae128c77
Christopher Robertson <christopherprobertson@gmail.com>
2020-06-22 22:10:41 +0100

    - Combined all of the .gitignores since having them with an initial name makes them obsolete.
    - Specifically added the removal of any .csv files since I do not want to release anyone's personal data.
    
    - Now have the project at the stage where it stores all of the data into a class named 'Person' and then assigns a name and a list of projects ready to access.

---
+++ sorter-project/sorter-project/person.py
@@ -0,0 +1,8 @@
+class Person(object):
+    """STORES THE NAME OF THE PERSON AND THEIR MODULES"""
+    
+
+    def __init__(self, name, projects):
+        self.name = name
+        self.projects = projects;
+
