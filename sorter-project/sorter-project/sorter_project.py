fb0f4707e310ff7dc02f1585fd6b6ec554250077
Christopher Robertson <christopherprobertson@gmail.com>
2020-06-26 19:19:36 +0100

    - Completed re-worked how data is stored and sorted. The data is sorted randomly before it is used.
    - I am also now using a dictionary instead of a class since it just makes mores sense and is simpler.
    
    - Need to re-introduce the checking for duplicate feature.

--- sorter-project/sorter-project/sorter_project.py
+++ sorter-project/sorter-project/sorter_project.py
@@ -17,25 +17,36 @@ from person import *
 weeks = 0
 projects_per_week = 0
 
-people = [] 
-all_projects = []
+global people, all_projects
 
 def main():
+    global people, all_projects
+
+    all_projects = []
+
     weeks = int(input("How many weeks will this be over the course of?\n"))
     projects_per_week = int(input("Amount of Projects Each Week?\n"))
     people = get_data()
-
-    all_projects = get_all_projects(people)
-
-    projects_per_week = sort_per_week(weeks, projects_per_week, all_projects)
-    save_schedule(projects_per_week)
+    
+    while(get_projects_length(people) > 0):
+        all_projects += get_projects_in_random(people)
 
 
-def save_schedule(projects_per_week):
+    projects = sort_per_week(weeks, projects_per_week, all_projects)
+    nested_projects = get_nested_remaining_projects(projects_per_week, all_projects)
+
+    save_schedule(projects, nested_projects)
+
+
+def save_schedule(projects_per_week, remaining_projects):
     with open('data/schedule.csv', 'w') as new_file:
         csv_writer = csv.writer(new_file)
         for week in projects_per_week:
             csv_writer.writerow(week)
+
+        for extra in remaining_projects:
+            csv_writer.writerow(extra)    
+
     print("Saved Schedule to:\ndata/schedule.csv")
 
 
@@ -44,97 +55,109 @@ def print_schedule(projects_each_week):
         print(projects)
 
 
-def sort_per_week(weeks, projects_per_week, all_projects):
+def sort_per_week(weeks, projects_per_week, projects_in_order):
     projects_each_week = []
-    
+
+    global people_used
+    people_used = []
+
     for i in range(0, weeks):
         projects = []
         projects.append("WEEK " + str(i + 1))
 
         for j in range(0, projects_per_week):
-            project = get_random_project(all_projects, projects)
-            if (project == None):
-                continue
-
-            projects.append(project)
+            projects.append(projects_in_order[0])
+            projects_in_order.pop(0)
 
         projects_each_week.append(projects)
 
     return projects_each_week
 
 
+def get_nested_remaining_projects(projects_per_week, remaining_projects):
+    nested_projects = []
+
+    for i in range(0, round(len(remaining_projects) % projects_per_week)):
+        projects = []
+        projects.append("REMAINDER " + str(i + 1))
+
+        for j in range(0, projects_per_week):
+            projects.append(remaining_projects[0])
+            remaining_projects.pop(0)
+
+        nested_projects.append(projects)
+    return nested_projects
+
+
 """Example of a Line: ['Christopher R.', 'Lego Mindstorm BS', 'COMP110 - SPACECHEM']"""
 def get_data():
-    people = []
-    amount_of_projects = 0
+    global people
+    people = {}
 
     with open ('data/test-data.csv', 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
 
         for line in csv_reader:
-            #Removing one since the first should always be a name
-            amount_of_projects += len(line) - 1
-            people.append(assign_person(line))
+            projects = []
+            name = line[0]
+
+            for cel in line:
+                if (cel != name and cel != ""):
+                    projects.append(cel)
+
+            people[name] = projects
 
     return people
-
-
-"""Sets and returns a Person class for grouping the data"""
-def assign_person(details):
-    name = details[0]
-    
-    details.remove(name)
-    person = Person(name, details)
-    return person
 
 
 """Returns the total amount of projects from everyone"""
 def get_projects_length(people):
     amount = 0;
     for person in people:
-        amount += len(person.projects)
+        amount += len(people.get(person))
 
     return amount
 
 
-"""Gets all of the projects in a list"""
-def get_all_projects(people):
+"""Gets a random project and then removes it from all of the projects"""
+def get_random_project():
+    if (len(people) <= 0):
+        return None
+
+    randomise_order(people)
+
+
+    return project
+
+
+def get_projects_in_random(people):
+    randomise_order(people)
+
     all_projects = []
-    for person in people:
-        for item in person.projects:
-            all_projects.append([person.name, item])
+    keys = randomise_dictionary_keys(people)
+
+    for person in keys:
+        if (len(people.get(person)) > 0):
+            project = get_random_person_project(people.get(person))
+            people.get(person).remove(project)
+            all_projects.append(person + ": " + project)
 
     return all_projects
 
+def randomise_dictionary_keys(dictionary):
+    keys = list(dictionary.keys())
+    random.shuffle(keys)
+    return keys
 
-"""Gets a random project and then removes it from all of the projects"""
-def get_random_project(all_projects, current_projects):
-    if (len(all_projects) <= 0):
-        return None
 
-    project = None
-    crash_counter = 0
+def get_random_person_project(person):
+    random_value = random.randrange(0, len(person))
+    return person[random_value]
 
-    while (project == None):
-        random_value = random.randrange(0, len(all_projects))
-        project = all_projects[random_value]
-
-        if (check_for_duplicate(project[0], current_projects)):
-            project = None
-            crash_counter += 1
-
-        if(crash_counter > len(all_projects)):
-            return None
-
-    all_projects.remove(project)
-
-    return project
+def randomise_order(people):
+    for person in people:
+        projects = people.get(person)
+        random.shuffle(projects)
+        people[person] = projects
     
-def check_for_duplicate(element, list):
-    for i in range(0, len(list)):
-        if element == list[i][0]:
-            return True
-
-    return False
-
 main();
